6a4f712486d419a59e1baee19c8b2a2acbf0dfd6
bekzodxudayberdiyev <khuyberdievdev@gmail.com>
2023-04-03 18:15:16 +0500

    some changes and question forms added

--- polls/urls.py
+++ polls/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
-from polls.views import homepage, questions_list, question_detail
+from polls.views import homepage, questions_list, question_detail, question_vote, question_add
 
 app_name = 'polls'
 
 urlpatterns = [
     path('', homepage, name='homepage'),
     path('questions/', questions_list, name='questions_list'),
-    path('questions/<int:question_id>/', question_detail, name='question_detail')
+    path('questions/<int:question_id>/', question_detail, name='question_detail'),
+    path('questions/<int:question_id>/vote/', question_vote, name='question_vote'),
+    path('questions/add/', question_add, name='question_add'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,9 @@
+from django.contrib import messages
+from django.contrib.messages import SUCCESS, ERROR
 from django.http import HttpResponse, Http404
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
 
+from polls.forms import QuestionForm
 from polls.models import Question, Choice
 
 
@@ -9,13 +12,6 @@ def homepage(request):
 
 
 def questions_list(request):
-    # without templates
-    # questions = Question.objects.all()
-    # response = ''
-    # for index, question in enumerate(questions):
-    #     response += f"{index + 1}. {question.text}<br>"
-    # return HttpResponse(f"Questions list here.<br>{response}")
-
     questions = Question.objects.all()
 
     context = {
@@ -26,22 +22,38 @@ def questions_list(request):
 
 
 def question_detail(request, question_id):
-    # without template
-    # try:
-    #     question = Question.objects.get(id=question_id)
-    # except Question.DoesNotExist:
-    #     raise Http404
-    # else:
-    #     return HttpResponse(f"Question text: {question.text}<br>pub_date: {question.pub_date}")
-    # question = get_object_or_404(Question, id=question_id)
-    # return HttpResponse(f"Question text: {question.text}<br>pub_date: {question.pub_date}")
-
     question = get_object_or_404(Question, id=question_id)
-    # choices = Choice.objects.filter(question=question)
 
     context = {
         "question": question,
-        # "choices": choices
     }
 
     return render(request, 'polls/question_detail.html', context=context)
+
+
+def question_vote(request, question_id):
+    choice = request.POST['choice']
+    question = get_object_or_404(Question, id=question_id)
+    try:
+        selected_choice = question.choices.get(pk=choice)
+    except Choice.DoesNotExist:
+        raise Http404("Choice doesn't exist.")
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        if selected_choice.is_true:
+            messages.add_message(request, SUCCESS, 'Your choice is correct.')
+            return HttpResponse('Your choice is correct.')
+        else:
+            messages.add_message(request, ERROR, 'Your choice is incorrect.')
+            return HttpResponse('Your choice is incorrect.')
+    # return redirect("polls:questions_list")
+
+
+def question_add(request):
+    form = QuestionForm(request.POST)
+    if request.method == "POST":
+        if form.is_valid():
+            form.save()
+            return redirect("polls:questions_list")
+    return render(request, 'polls/add_question.html', {"form": form})
